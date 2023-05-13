d927083f63e09dae9c81d8a6e960223fed86aa13
ashim-ashraf <ashimashraf@gmail.com>
2023-05-14 00:58:52 +0530

    [Fix] code formatting

--- client/src/components/Vendor-Components/VendorLogin.jsx
+++ client/src/components/Vendor-Components/VendorLogin.jsx
@@ -21,17 +21,6 @@ function VendorLogin() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  let x = {
-    apiKey: "AIzaSyBvvMM7OOOoA5zkyKRzbqYMytllG1j2frU",
-    authDomain: "yummers-7c3d1.firebaseapp.com",
-    projectId: "yummers-7c3d1",
-    storageBucket: "yummers-7c3d1.appspot.com",
-    messagingSenderId: "21777330572",
-    appId: "1:21777330572:web:64a4fef0847875a99566a9",
-    measurementId: "G-E7VVBKSL3H"
-  }
-  console.log("config ", x)
-
 function resendotp() {
   const phoneNumber = "+" + phone;
   onSigninssubmit(phoneNumber)
