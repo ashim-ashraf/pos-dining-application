652b3b39cb658489eb5b57c5ba5ae4f6bae17f54
ashim-ashraf <ashimashraf@gmail.com>
2023-05-13 21:41:21 +0530

    [fix] code formatting

--- client/src/firebase.js/firebase.jsx
+++ client/src/firebase.js/firebase.jsx
@@ -7,13 +7,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvvMM7OOOoA5zkyKRzbqYMytllG1j2frU",
-  authDomain: "yummers-7c3d1.firebaseapp.com",
-  projectId: "yummers-7c3d1",
-  storageBucket: "yummers-7c3d1.appspot.com",
-  messagingSenderId: "21777330572",
-  appId: "1:21777330572:web:64a4fef0847875a99566a9",
-  measurementId: "G-E7VVBKSL3H",
+  apiKey: ""+process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
